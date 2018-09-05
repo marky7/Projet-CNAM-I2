6583e4c4d029fd2fb5299ec834491fa9006d398b
Florian M <florian.marc@gmx.com>
2018-09-05 23:19:04 +0200

    a+ tout le monde

--- arduino-nano/nodejs_connect_amqp/index.js
+++ arduino-nano/nodejs_connect_amqp/index.js
@@ -77,44 +77,6 @@ var acquisition = new Acquisition([gaz1,gaz2,temperatureHumidity],tags);
 var acquisitionToString = JSON.stringify(acquisition);
 console.log(acquisitionToString);
 
-//**************************************** ALEXIS ****************************************//
-// Message à envoyer
-// Instanciation des données reçues de l'arduino
-// Tag bluetooth du colis
-var messageToSent = "157458";
-// Chargement du message
-
-// Nous ne pouvons pas anticiper comment les mesures seront envoyées
-// Sinon, il faudrait reconnaître le type de colis que c'est avant de push les mesures
-// sur le canal de communication AMQP
-// Nous allons donc trancher : chaque message envoyé au serveur J2E par le biais d'AMQP
-// se composera toujours d'un ID (le colis)
-// Avec celles-ci, nous ferons un message de type:
-// idColis;NomMesure1:ValeurMesure1;NomMesure2:ValeurMesure2;NomMesure3:ValeurMesure3...
-// Si une viande a besoin qu'on contrôle la température, l'humidité et le méthane
-// Cela donnera (On admet que la viande possède le numéro de colis 000001):
-// var messageAEnvoyer : "000001;Temperature:19.4;Humidity:94;CH4:54"
-
-// ICI, traitement des mesures reçues par l'Arduino, tout à FALSE
-var TemperatureExist = false;
-var HumidityExist = false
-var NH3Exist = false;
-var CommentNO2Exist = false;
-var C3H8Exist = false;
-var C4H10Exist = false;
-var CH4Exist = false;
-var H2Exist = false;
-var C2H50HExist = false;
-
-// Si une température est reçue, tu passes à true + rajout de la valeur dans le string
-// If Arduino m'envoie une mesure de température then Temperature = "37.00"
-if (temperatureExist =true){
-var Temperature = "Temperature:TU REMPLACES CETTE PHRASE EN MAJUSCULE PAR LA VALEUR RECU PAR LARDUINO";
-// On rajoute toujours un ";" dans un appel de concat, comme ça, il n'y aura que des points virgules ajoutés à la string
-// uniquement si une mesure va être insérée dans le message
-messageToSent.concat(";",Temperature);
-// Notre message sera donc messageToSent ="1574581;Temperature:37.00"
-}
 
 // Apparement, require ne s'utilise plus avec ECMAScript, il faudrait faire un import.
 var amqp = require('amqplib/callback_api');
@@ -128,8 +90,9 @@ var queue = 'FileDesMessages';
 // Insertion
 ch.assertQueue(queue, {durable: false});
 // Envoie du message (messageToSent)
-ch.sendToQueue(queue, Buffer.from(messageToSent));
-console.log(" [x] Sent %s", messageToSent);
+console.log('Envoie du message... -------------------------------------------------------------')
+ch.sendToQueue(queue, Buffer.from(acquisitionToString));
+console.log(" [x] Sent %s ---------------------------------------------------------------- /n", messageToSent);
 });
 setTimeout(function() { conn.close(); process.exit(0) }, 500);
 });
