7c6207add302d5dd30dc9318a7384f7a501c2ac4
Florian M <florian.marc@gmx.com>
2018-09-06 13:17:26 +0200

    Add documentation

---
+++ documentation/generate_api_rest.sh
@@ -0,0 +1,9 @@
+# Mettre à jour l'API REST avec le nouveau modèle de données
+# ATTENTION : Le script start ne doit pas avoir été lancé
+
+# Démarrer Fabric
+cd ~/fabric-dev-servers/fabric-scripts/hlfv1/composer
+docker-compose start
+
+# Générer la nouvelle API à partir du fichier CTO
+composer-rest-server -c admin@package-traceability-network -n never -w true
