c0b8f62162b91c78eb803a05ea05238f444da33f
Florian M <florian.marc@gmx.com>
2018-10-13 00:30:32 +0200

    Update generate_api script

--- documentation/generate_api_rest.sh
+++ documentation/generate_api_rest.sh
@@ -1,5 +1,6 @@
 # Mettre à jour l'API REST avec le nouveau modèle de données
 # ATTENTION : Le script start ne doit pas avoir été lancé
+./../stop.sh
 
 # Démarrer Fabric
 cd ~/fabric-dev-servers/fabric-scripts/hlfv1/composer
